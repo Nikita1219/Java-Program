d42022f72e7c74975c48f2d9a4744bae404053ff
Nikita1219 <47317610+Nikita1219@users.noreply.github.com>
2019-02-13 22:34:26 -0800

    Add files via upload

---
+++ Rectangle1.java
@@ -0,0 +1,19 @@
+public class Rectangle1 implements Shape
+{
+	private double width;
+	private double height;
+	
+	public Rectangle1(double w,double h)
+	{
+		this.width=w;
+		this.height=h;
+	}
+	public void draw()
+	{
+		System.out.println("Drawing Rectangle");
+	}
+	public double getArea()
+	{
+		return this.height*this.width;
+	}
+}
